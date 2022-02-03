94e92818907ea49c0ffb99d5fc285c20159bbf89
corzogac <gerald.corzo@gmail.com>
2022-02-04 00:13:13 +0100

    temp

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application=Flask(__name__)
 @application.get('/')
 def main():
     #print('hello from Holland')
-    person = [{'id':1,'name': 'Alice', 'username': "1986"},{'id':2,'name': 'Gerald', 'username': "Corzo"}]
+    person = [{'id':1,'name': 'Alice2', 'username': "1986"},{'id':2,'name': 'Gerald', 'username': "Corzo"}]
     return jsonify(person) # Returns HTTP Response with {"hello": "world"}
 
 
