0be025035fd96c9dbf1877e553f82143afc2cd4c
corzogac <gerald.corzo@gmail.com>
2022-02-20 19:58:52 +0100

    Update link

--- TestURL.py
+++ TestURL.py
@@ -1,5 +1,12 @@
 import urllib.request
-#with urllib.request.urlopen('http://hillapp-env.eba-pwi4s6pp.us-east-1.elasticbeanstalk.com') as response:
-with urllib.request.urlopen('https://jsonplaceholder.typicode.com/users') as response:
+with urllib.request.urlopen('http://hillapp-env.eba-pwi4s6pp.us-east-1.elasticbeanstalk.com/users') as response:
+#with urllib.request.urlopen('https://jsonplaceholder.typicode.com/users') as response:
    html = response.read()
+   header=response.headers
    print(html)
+   print(header)
+
+
+   # Always start with
+   # export FLASK_APP="application.py"  
+   #flask run
