5ce41732ad89c68139082b1c757fb559d0f92cbf
corzogac <gerald.corzo@gmail.com>
2022-02-20 19:59:05 +0100

    More data

--- application.py
+++ application.py
@@ -5,8 +5,14 @@ application=Flask(__name__)
 @application.route('/users')
 def main():
     #print('hello from Holland')
-    person = [{'id':1,'name': 'Alice2', 'username': "1986"},{'id':2,'name': 'Gerald', 'username': "Corzo"}]
+    person = [{'id':1,'name': 'Alice2', 'username': "1986",'Site':"https://betterprogramming.pub/data-visualization-with-swiftui-radar-charts-64124aa2ac0b"},
+    {'id':2,'name': 'Gerald', 'username': "Corzo",'Site':"https://bookauthority.org/books/new-swiftui-books"}]
     return jsonify(person) # Returns HTTP Response with {"hello": "world"}
 
+
+
+@application.route('/')
+def PageLand():
+    return "Welcome to my test stie"
 
 #export FLASK_APP
